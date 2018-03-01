ad6af68414b1b30ed327b79965e4e89f1db508e3
Daniel O'Neill <daniel.oneill@flexdrive.com>
2018-03-01 10:31:27 -0500

    Allow unknown elements in validation

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const Joi = require('joi')
+const Hoek = require('hoek')
 
 module.exports = function({ joiOptions }) {
   const seneca = this
+  const defaults = { allowUnknown: true }
+  const validationOptions = Hoek.applyToDefaults(defaults, joiOptions)
 
   function validate_schema_inward(ctx, data) {
     if (ctx.actdef.raw.schema$) {
       const result = Joi.validate(
         ctx.seneca.util.clean(data.msg),
         ctx.actdef.raw.schema$,
-        joiOptions
+        validationOptions
       )
 
       if (result.error) {
